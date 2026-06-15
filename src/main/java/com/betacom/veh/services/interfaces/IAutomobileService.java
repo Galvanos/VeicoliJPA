140f2245e180de8ac53c7be5a616055942b9fa99
Marco Scarpa <marco.scarpa@tiscali.it>
2026-06-15 16:27:43 +0200

    added DTO return in service so is possible tro know how the just created/updated object is and how the just deleted object was

--- src/main/java/com/betacom/veh/services/interfaces/IAutomobileService.java
+++ src/main/java/com/betacom/veh/services/interfaces/IAutomobileService.java
@@ -6,9 +6,9 @@ import com.betacom.veh.dto.input.AutomobileRequest;
 import com.betacom.veh.dto.output.AutomobileDTO;
 
 public interface IAutomobileService {
-	void insert(AutomobileRequest req) throws Exception;
-	void update(AutomobileRequest req) throws Exception;
-	void delete(Integer id) throws Exception;
+	AutomobileDTO insert(AutomobileRequest req) throws Exception;
+	AutomobileDTO update(AutomobileRequest req) throws Exception;
+	AutomobileDTO delete(Integer id) throws Exception;
 	
 	List<AutomobileDTO> list() throws Exception;
 	AutomobileDTO getById(Integer id) throws Exception;
