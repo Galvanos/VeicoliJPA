9f0deedc3211e963e407f1463e0ad0a5387a1160
Luca Ongaro <ongaro.lol.luca@gmail.com>
2026-06-15 10:37:02 +0200

    add initial files

---
+++ src/main/java/com/betacom/veh/VeicoliJpaApplication.java
@@ -0,0 +1,13 @@
+package com.betacom.veh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeicoliJpaApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(VeicoliJpaApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/betacom/veh/VeicoliJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom.veh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeicoliJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
