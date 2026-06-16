09a097628a4d11cc779acf76d1602f39a9079057
iresha-warnakulasuriya <dilhanimadu2002@gmail.com>
2026-06-16 17:09:21 +0200

    Added classes: VeicoloController, VeicoloImplementation, VeicoloMap. Modified class IVeicoloService

---
+++ src/main/java/com/betacom/veh/controllers/VeicoloController.java
@@ -0,0 +1,35 @@
+package com.betacom.veh.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.betacom.veh.dto.output.VeicoloDTO;
+import com.betacom.veh.services.interfaces.IVeicoloService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/rest/veicolo")
+public class VeicoloController {
+	
+	private final IVeicoloService veicoloService;
+	
+	@GetMapping("/list")
+	public ResponseEntity<List<VeicoloDTO>> list() throws Exception{
+		return ResponseEntity.ok(veicoloService.findAll());
+	}
+	
+	@GetMapping("getById")
+	public ResponseEntity<VeicoloDTO> getById(@RequestParam (required = true) Integer id) throws Exception{
+		return ResponseEntity.ok(veicoloService.findById(id));
+	}
+
+}
--- src/main/java/com/betacom/veh/services/interfaces/IVeicoloService.java
+++ src/main/java/com/betacom/veh/services/interfaces/IVeicoloService.java
@@ -1,5 +1,11 @@
 package com.betacom.veh.services.interfaces;
 
+import java.util.List;
+
+import com.betacom.veh.dto.output.VeicoloDTO;
+
 public interface IVeicoloService {
 
+	List<VeicoloDTO> findAll();
+	VeicoloDTO findById(Integer id);
 }
