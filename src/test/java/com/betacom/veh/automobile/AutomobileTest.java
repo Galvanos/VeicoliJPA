31120f2d7776bc4c41365fe9213f54be1b091574
Luca Ongaro <48388970+Galvanos@users.noreply.github.com>
2026-06-23 17:20:31 +0200

    add tests for majority of automobile code

--- src/test/java/com/betacom/veh/automobile/AutomobileTest.java
+++ src/test/java/com/betacom/veh/automobile/AutomobileTest.java
@@ -1,13 +1,16 @@
 package com.betacom.veh.automobile;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
+import java.util.List;
 
-import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -20,13 +23,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import com.betacom.veh.dto.input.AutomobileRequest;
+import com.betacom.veh.dto.output.AutomobileDTO;
 import com.betacom.veh.dto.output.ResponseDTO;
-import com.betacom.veh.models.Automobile;
 import com.betacom.veh.models.TipoAlimentazione;
 import com.betacom.veh.models.TipoAlimentazioneId;
 import com.betacom.veh.repositories.ITipoAlimentazioneRepository;
 
 import lombok.extern.slf4j.Slf4j;
+import tools.jackson.core.JacksonException;
+import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
 
 @Slf4j
@@ -44,10 +49,7 @@ public class AutomobileTest {
 	private ITipoAlimentazioneRepository typeRepo;
 	
 	
-	
-	private Automobile insertedCar;
-	
-	private AutomobileRequest createValidAutomobileCreateRequest() {
+	private AutomobileRequest createValidAutomobileCreateRequest(String targa) {
 		return AutomobileRequest.builder(
 				).annoProduzione(2020)
 				.categoria("BERLINA_MEDIA")
@@ -55,12 +57,21 @@ public class AutomobileTest {
 				.colore("giallo")
 				.marca("Nissan")
 				.modello("micra")
-				.targa("RR456GG")
+				.targa(targa)
 				.tipoAlimentazione("BENZINA")
 				.numeroRuote(4)
 				.numeroPorte(5)
 				.build();
 	}
+	private AutomobileDTO insertAutomobile(String targa) throws Exception{
+		AutomobileRequest request = createValidAutomobileCreateRequest(targa);
+		MvcResult result = mockMvc.perform(post("/rest/automobile/create")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request))
+					).andExpect(status().isOk())
+					.andReturn();
+		return objectMapper.readValue(result.getResponse().getContentAsString(), AutomobileDTO.class);
+	}	
 	
 	private MvcResult getPostErrorResult(String uri, AutomobileRequest request) throws Exception{
 		return mockMvc.perform(post(uri)
@@ -77,14 +88,14 @@ public class AutomobileTest {
 		tipo.setTipoAlimentazioneId(TipoAlimentazioneId.builder()						//creando tipo alimentazione
 				.tipoAlimentazione("BENZINA").tipoVeicolo("AUTOMOBILE").build());		//creando tipo alimentazione
 		typeRepo.save(tipo);															//creando tipo alimentazione
-		AutomobileRequest request = createValidAutomobileCreateRequest();		
+		AutomobileRequest request = createValidAutomobileCreateRequest("SS667BV");		
 		try {
-			MvcResult result = mockMvc.perform(post("/rest/automobile/create")
+			mockMvc.perform(post("/rest/automobile/create")
 					.contentType(MediaType.APPLICATION_JSON)
 					.content(objectMapper.writeValueAsString(request))
 					).andExpect(status().isOk())
 					.andReturn();
-			insertedCar = objectMapper.readValue(result.getResponse().getContentAsString(), Automobile.class);
+
 		} catch (Exception e) {
 			log.error("Error in create: " + e.getMessage());
 		}		
@@ -93,50 +104,139 @@ public class AutomobileTest {
 	@Test
 	@Order(2)
 	public void createAutomobileErrorTest() throws Exception{
-		AutomobileRequest request = createValidAutomobileCreateRequest();
-		request.setAnnoProduzione(1992);
-		MvcResult result = getPostErrorResult("/rest/automobile/create", request);
-				
+		AutomobileRequest request = createValidAutomobileCreateRequest("RRRRRRRRRR");	//test targa formato errato
+		MvcResult result = getPostErrorResult("/rest/automobile/create", request);			
+		
 		String json = result.getResponse().getContentAsString();
 		ResponseDTO dto = objectMapper.readValue(json, ResponseDTO.class);
+
+		log.debug("error: " + dto.getMsg() + ", expected: " + "'targa invalida'");		
+		request.setTarga("RT789FV");		//reset valore
 		
-		log.debug("error: " + dto.getMsg() + ", expected: " + "'anno invalido'");
+		request.setAnnoProduzione(1992);												//test anno fuori range
+		result = getPostErrorResult("/rest/automobile/create", request);
+				
+		json = result.getResponse().getContentAsString();
+		dto = objectMapper.readValue(json, ResponseDTO.class);
 		
-		request.setAnnoProduzione(LocalDateTime.now().getYear());
-		request.setTarga("RRRRRRRRRR");
+		log.debug("error: " + dto.getMsg() + ", expected: " + "'anno invalido'");		
+		request.setAnnoProduzione(LocalDateTime.now().getYear());	//reset valore
 		
+		request.setTarga("SS667BV");													//test targa duplicata
+		result = getPostErrorResult("/rest/automobile/create", request);			
+		
+		json = result.getResponse().getContentAsString();
+		dto = objectMapper.readValue(json, ResponseDTO.class);
+
+		log.debug("error: " + dto.getMsg() + ", expected: " + "'targa già presente'");
+		request.setTarga("RT789FV");		//reset valore
+		
+		request.setCategoria("TORO_SEDUTO");											//test categoria non esistente
+		result = getPostErrorResult("/rest/automobile/create", request);			
+		
+		json = result.getResponse().getContentAsString();
+		dto = objectMapper.readValue(json, ResponseDTO.class);
+
+		log.debug("error: " + dto.getMsg() + ", expected: " + "'categoria non valida'");
+		request.setCategoria("BERLINA_MEDIA");	//reset valore
+		
+		request.setTipoAlimentazione("STRACCHINO");											//test tipo alimentazione non esistente
 		result = getPostErrorResult("/rest/automobile/create", request);
 		
 		json = result.getResponse().getContentAsString();
 		dto = objectMapper.readValue(json, ResponseDTO.class);
-		
 
-		log.debug("error: " + dto.getMsg() + ", expected: " + "'targa invalida'");
+		log.debug("error: " + dto.getMsg() + ", expected: " + "'tipo alimentazione non valido'");
+		request.setTipoAlimentazione("BENZINA");	//reset valore
+		
+		request.setNumeroRuote(-1);											//test numero ruote
+		result = getPostErrorResult("/rest/automobile/create", request);
+		
+		json = result.getResponse().getContentAsString();
+		dto = objectMapper.readValue(json, ResponseDTO.class);
+
+		log.debug("error: " + dto.getMsg() + ", expected: " + "'numero ruote non valido'");
+		request.setNumeroRuote(4);		//reset valore
+		
+		request.setNumeroPorte(333);											//test numero porte
+		result = getPostErrorResult("/rest/automobile/create", request);
+		
+		json = result.getResponse().getContentAsString();
+		dto = objectMapper.readValue(json, ResponseDTO.class);
+
+		log.debug("error: " + dto.getMsg() + ", expected: " + "'numero porte non valido'");
+		request.setNumeroPorte(7);		//reset valore
 	}
 	
 	@Test
 	@Order(3)
-	public void updateAutomobileTest() throws Exception{
-		AutomobileRequest request = AutomobileRequest.builder().id(insertedCar.getId()).cc(250).targa(insertedCar.getTarga()).build();
-		MvcResult result = mockMvc.perform(post("/rest/automobile/update")				
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(request))
-				).andExpect(status().isOk())
-				.andReturn();
-		String json = result.getResponse().getContentAsString();
-		ResponseDTO dto = objectMapper.readValue(json, ResponseDTO.class);
-		
-		log.debug("update result: " + dto);
+	public void updateAutomobileTestSameTarga() throws Exception{
+		AutomobileDTO insertedCar = insertAutomobile("VV333FF");
+		updateAutomobile(insertedCar.getTarga(), insertedCar.getId());
 	}
 	
 	@Test
 	@Order(4)
-	public void deleteAutomobileTest() throws Exception{
+	public void updateAutomobileTestDifferentTarga() throws Exception{
+		AutomobileDTO insertedCar = insertAutomobile("XX333FF");
+		updateAutomobile("FF999NW", insertedCar.getId());
+	}
+	
+	private void updateAutomobile(String targa, Integer id) throws Exception{
+		AutomobileRequest request = AutomobileRequest.builder().id(id).cc(250).targa(targa).build();
+		try {
+			MvcResult result = mockMvc.perform(patch("/rest/automobile/update")				
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(objectMapper.writeValueAsString(request))
+					).andExpect(status().isOk())
+					.andReturn();
 
+			String json = result.getResponse().getContentAsString();
+			ResponseDTO dto = objectMapper.readValue(json, ResponseDTO.class);
+			
+			log.debug("update result: " + dto);
+		}catch (Exception e) {
+			log.debug("Errore:" + e.getMessage());
+		}
+	}
+	
+	@Test
+	@Order(5)
+	public void deleteAutomobileTest() throws Exception{
+		AutomobileDTO insertedCar = insertAutomobile("DD333FF");
 		mockMvc.perform(delete("/rest/automobile/delete/" + insertedCar.getId()))
-        	.andExpect(status().isOk())
-        	.andExpect(jsonPath("$.msg").exists());
-		
+        	.andExpect(status().isOk());
 		log.debug("Auto deleted");
+	}
+	
+	@Test
+	@Order(6)
+	public void getAutomobileByIdTest() throws Exception{
+		AutomobileDTO insertedCar = insertAutomobile("HH223FF");
+		MvcResult result = mockMvc.perform(get("/rest/automobile/getById").param("id", insertedCar.getId().toString()))
+			.andExpect(status().isOk())
+			.andReturn();
+		
+		String json = result.getResponse().getContentAsString();
+		insertedCar = objectMapper.readValue(json, AutomobileDTO.class);		
+		log.debug(insertedCar.toString());
+	}
+	
+	@Test
+	@Order(7)
+	public void getAllAutomobile() throws Exception{
+		MvcResult result = mockMvc.perform(get("/rest/automobile/list"))
+	            .andExpect(status().isOk())
+	            .andReturn();
+		  
+		String json = result.getResponse().getContentAsString();
+		
+		List<AutomobileDTO> lS = objectMapper.readValue(
+	            json,
+	            new TypeReference<List<AutomobileDTO>>() {}
+	    );
+		
+		assertFalse(lS.isEmpty());
+		lS.forEach(s -> log.debug(s.toString()));
 	}
 }
